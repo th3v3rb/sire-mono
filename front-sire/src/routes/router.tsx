17378df5038ea2991c46a9073981cf7ec7d66c5a
Hugo Penayo <125562572+th3v3rb@users.noreply.github.com>
2025-01-11 18:21:03 -0300

    .

--- front-sire/src/routes/router.tsx
+++
@@ -1,9 +0,0 @@
-import {createBrowserRouter} from "react-router-dom";
-import guestRoutes from "./guest.routes.tsx";
-
-
-export const browserRoutes = createBrowserRouter(
-    [
-        ...guestRoutes
-    ]
-)
